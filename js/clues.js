f8a00053bf9c9a1c0cd3c1aa5e1add315c42540d
Marcus Christie <marc.christie@gmail.com>
2020-10-31 16:35:09 -0400

    clues and animations

--- js/clues.js
+++ js/clues.js
@@ -1,6 +1,58 @@
 const clues = {
   1: {
-    answer: "boo",
-    next: "Here's the next clue",
+    answer: "frankenstein",
+    next: "when he needs an electrical jolt this is where he gets a lightning bolt",
+  },
+  2: {
+    answer: "frankenstein",
+    next: "having a friend like frankie is the nicest; you can plug this into him when you need to charge your devices",
+  },
+  3: {
+    answer: "scary clown",
+    next: "these look a little like the nose of a clown; under them is where the next clue is found",
+  },
+  4: {
+    answer: "scary clown",
+    next: "the scary clown keeps one for the rain; or is it to keep dry in the drain",
+  },
+  5: {
+    answer: "haunted house",
+    next: "every haunted house needs a haunted attic; finding your way there would be a smart tactic",
+  },
+  6: {
+    answer: "haunted house",
+    next: "is there a creepier place than under the stairs? Be careful that you do not get too many scares",
+  },
+  7: {
+    answer: "ghostbusters",
+    next: "who you gonna call? that is all",
+  },
+  8: {
+    answer: "ghostbusters",
+    next: "i aint fraid of no ghost; but i am afraid of this thing that makes toast",
+  },
+  9: {
+    answer: "halloween",
+    next: "if you forget on what day halloween falls, you can check this on the wall",
+  },
+  10: {
+    answer: "halloween",
+    next: "do not forget to do this after eating all those sweets; you would not want to lose all your teeth",
+  },
+  11: {
+    answer: "jackolantern",
+    next: "you need one of these to make ole jack glow; when you find it the next clue you will know",
+  },
+  12: {
+    answer: "jackolantern",
+    next: "this is a good place for jack to be a greeter; and beam hello to each trick-or-treater",
+  },
+  13: {
+    answer: "count dracula",
+    next: "a sweet red liquid but not the count’s favorite i think; it is one you use to flavor your drink",
+  },
+  14: {
+    answer: "count dracula",
+    next: "to keep his fangs in place the count uses one before bed; he climbs into his coffin and sleeps like the dead",
   },
 };
